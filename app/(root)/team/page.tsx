713a0f2e5c3d7e935ca3e925b2be7c5ba32d927a
Glenn Hensley III <glenniii.dev@gmail.com>
2025-10-08 12:00:57 -0400

    Added Rolleen Spotlight

--- app/(root)/team/page.tsx
+++ app/(root)/team/page.tsx
@@ -2,7 +2,7 @@ import TeamMember from "@/components/cards/TeamMember";
 
 export default function page() {
   return (
-    <section className="flex flex-row flex-wrap items-center justify-center p-10 lg:px-20 lg:py-15 text-(--oxford-blue) gap-6">
+    <section className="flex flex-row flex-wrap items-center justify-center p-10 lg:py-15 text-(--oxford-blue) gap-6">
       <div className="flex flex-col mb-6 justify-start w-full">
         <h1 className="text-4xl font-bold mb-4 text-(--chambray) text-shadow-md">Our Team</h1>
         <h3 className="text-lg mb-4">Meet the passionate individuals behind Pixel Pulse. Our diverse team of students and young professionals brings together expertise in STEM research, journalism, design, and technology to create engaging and informative content.</h3>
@@ -12,7 +12,7 @@ export default function page() {
       <TeamMember image="https://dzoxjkfkahmyylvgiibw.supabase.co/storage/v1/object/public/team-images/1744584649464-IMG_3289.webp" name="Soha" role="Founder & President" bio="Hi there! I'm Soha, the founder and president of Pixel Pulse. I'm currently pursuing STEM and planning to major in either computer science or data science (still deciding!) at university. I truly hope Pixel Pulse grows into a valuable platform for both STEM students and beyond! I love reading and writing. :)" />
       
       {/* ROLLEEN */}
-      <TeamMember image="/Rollean.webp" name="Rolleen" role="Vice President" bio="I'm Rolleen (pronounced row-lean)! I'm a high school student with a passion for chemistry, looking to pursue Chemical Engineering. I'm a big music nerd but I'm also a debater outside of Pixel Pulse! Glad to be part of this team as the research lead for Chemistry and Physics." />
+      <TeamMember image="/Rollean.webp" name="Rolleen" role="Vice President" bio="Hai my name is Rolleen! I am Pixel Pulse's Vice President and a long-term member. I am very passionate about environmental sciences and hope to pursue environmental engineering and public advocacy in the future. Outside of STEM, I am a big music person, so if you want any music suggestions I am the girl to go to. My favorite music artists consist of Jeff Buckley, Laufey, and Beabadoobee just to name a few! I am so grateful to be part of this team." />
       
       {/* DARREN */}
       <TeamMember image="https://cdn.glitch.global/e935e5cb-fc33-4f8d-a0d2-76cc83905dc9/darren.png?v=1739814810086" name="Darren" role="Graphic Designer + Admin Secretary" bio="Hi, I'm Darren, a high school student with experience in coding, web design, and graphic design. I'm passionate about technology and plan to pursue a computer science or software engineering career. Outside of tech, I enjoy both playing and watching sports, especially soccer and tennis." />
--- components/cards/TeamMember.tsx
+++ components/cards/TeamMember.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image"
 
 export default function TeamMember({ image, name, role, bio }: { image: string; name: string; role: string; bio: string; }) {
+
   return (
-    <div className="flex flex-col space-y-3 p-8 items-start bg-(--stardust-white) rounded-lg text-(--silver-lake-blue) max-w-80 min-w-60 w-auto h-135 shadow-xl hover:shadow-2xl transform hover:scale-105 transition duration-300">
+    <div className="flex flex-col space-y-3 p-8 items-start bg-(--stardust-white) rounded-lg text-(--silver-lake-blue) max-w-90 min-w-60 w-auto h-135 shadow-xl hover:shadow-2xl transform hover:scale-105 transition duration-300">
       <Image src={!image ? "/profile.png" : image} alt={name} width={96} height={96} className="w-24 h-24 mb-5 rounded-full border-3 border-(--chambray) self-center object-cover" />
       <h3 className="text-2xl font-bold text-(--oxford-blue) self-center">{name}</h3>
       <h3 className="text-lg font-bold text-(--chambray) self-center">{role}</h3>
@@ -10,3 +11,4 @@ export default function TeamMember({ image, name, role, bio }: { image: string; 
     </div>
   )
 }
+
